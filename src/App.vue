4502c312396bcdf0f5239aee6893f74bd1cc2fdb
ikool009 <ikool009@hotmail.com>
2020-12-03 12:40:12 +0700

    -

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Kanban/>
+    <Kanban :data="data"/>
   </div>
 </template>
 
@@ -12,6 +12,27 @@ export default {
   name: 'App',
   components: {
     Kanban
+  },
+  data(){
+    return {
+      data:[
+        {
+          name:"todo",
+          color:"#F5B271",
+          tasks:[]
+        },
+         {
+          name:"doing",
+          color:"#BEA771",
+          tasks:[]
+        },
+         {
+          name:"done",
+          color:"#1B796F",
+          tasks:[]
+        }
+      ]
+    }
   }
 }
 </script>
--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -1,19 +1,61 @@
 <template>
-    <div class="kanban">
-        Kanban
+  <div class="kanban">
+    <div
+      class="column"
+      :style="{ backgroundColor: column.color }"
+      v-for="(column, index) in data"
+      :key="index"
+    >
+        <div class="column-header">
+            {{column.name}}
+        </div>
+
+        <div class="column-body">
+
+        </div>
+
+     
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    data: Array,
+  },
+};
 </script>
 
 <style>
-.kanban{
-    width: 100%;
-    height: 100%;
-    background-color: bisque;
+.kanban {
+  width: 100%;
+  height: 100%;
+  background-color: bisque;
+}
+.column {
+  height: 600px;
+  width: 300px;
+  border-radius: 10px;
+  display: inline-block;
+  margin: 25px;
+  padding: 10px;
+  -webkit-box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.75);
+}
+.column-header{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    font-size: 32px;   
+    font-weight: bold;
+}
+.column-body{
+    height: calc(100% - 60px);
+    border-radius: 10px;
+    padding: 5px;
+    background-color: #ffffff7c;
 }
 </style>
